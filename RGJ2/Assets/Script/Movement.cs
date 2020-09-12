53f04ea2280234d029e45067d990bf02f78bc687
Henrique <riquesouzalima11@gmail.com>
2020-09-12 18:03:43 -0300

    move

--- RGJ2/Assets/Script/Movement.cs
+++ RGJ2/Assets/Script/Movement.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public Vector3 finalPosition;
-    public Vector3 dir;
 
+    private Vector3 finalPosition;
+    private Vector3 dir;
+    public Player player;
+    public enum Player { Player1, Player2};
+
+    private string horizontal;
+    private string vertical;
     // Start is called before the first frame update
     void Start()
     {
         finalPosition = transform.position;
+
+        switch ( player)
+        {
+            case Player.Player1:
+                horizontal = "Horizontal_1";
+                vertical = "Vertical_1";
+                break;
+            case Player.Player2:
+                horizontal = "Horizontal_2";
+                vertical = "Vertical_2";
+                break;
+        }
     }
 
     // Update is called once per frame
@@ -21,17 +38,22 @@ public class Movement : MonoBehaviour
         {
             transform.position = finalPosition;
 
-            dir.x = Input.GetAxisRaw("Horizontal");
-            dir.y = Input.GetAxisRaw("Vertical");
+            dir.x = Input.GetAxisRaw(horizontal);
+            dir.y = Input.GetAxisRaw(vertical);
 
             if (dir.x != 0)
             {
                 dir.y = 0;
                 RaycastHit2D hit;
-                hit = Physics2D.Raycast(transform.position,dir,1f);
+                hit = Physics2D.Raycast(transform.position +  new Vector3(0.6f,0.6f,0.6f),dir,1f);
                 
                 
+
                 if(!hit)
+                {
+                    finalPosition = finalPosition + dir;
+                }
+                else if(hit.transform.gameObject.tag == "Ground")
                 {
                     finalPosition = finalPosition + dir;
                 }
@@ -42,13 +64,18 @@ public class Movement : MonoBehaviour
             {
                 dir.x = 0;
                 RaycastHit2D hit;
-                hit = Physics2D.Raycast(transform.position, dir, 1f);
+                hit = Physics2D.Raycast(transform.position + new Vector3(0.6f, 0.6f, 0.6f), dir, 1f);
 
                 if (!hit)
                 {
-
                     finalPosition = finalPosition + dir;
                 }
+                else if (hit.transform.gameObject.tag == "Ground")
+                {
+                    finalPosition = finalPosition + dir;
+                }
+                
+
             }
 
 
@@ -58,5 +85,11 @@ public class Movement : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, finalPosition, 5 * Time.deltaTime);
         
         
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.transform.gameObject.tag);
     }
 }
