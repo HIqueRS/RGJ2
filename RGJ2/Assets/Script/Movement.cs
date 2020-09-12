bf62a4d9eeb3a4d9e77e092729787aae48e5e1d0
Henrique <riquesouzalima11@gmail.com>
2020-09-12 16:54:12 -0300

    Movement and colision

---
+++ RGJ2/Assets/Script/Movement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Vector3 finalPosition;
+    public Vector3 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if(Vector3.Distance(finalPosition,transform.position) < 0.2)
+        {
+            transform.position = finalPosition;
+
+            dir.x = Input.GetAxisRaw("Horizontal");
+            dir.y = Input.GetAxisRaw("Vertical");
+
+            if (dir.x != 0)
+            {
+                dir.y = 0;
+                RaycastHit2D hit;
+                hit = Physics2D.Raycast(transform.position,dir,1f);
+                
+                
+                if(!hit)
+                {
+                    finalPosition = finalPosition + dir;
+                }
+               
+                
+            }
+            else if(dir.y != 0)
+            {
+                dir.x = 0;
+                RaycastHit2D hit;
+                hit = Physics2D.Raycast(transform.position, dir, 1f);
+
+                if (!hit)
+                {
+
+                    finalPosition = finalPosition + dir;
+                }
+            }
+
+
+
+        }
+       
+        transform.position = Vector3.MoveTowards(transform.position, finalPosition, 5 * Time.deltaTime);
+        
+        
+    }
+}
