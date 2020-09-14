8dbf70a115edf5d6753a6c7917908e0fbc5f80e3
Henrique <riquesouzalima11@gmail.com>
2020-09-14 20:14:58 -0300

    Menu

---
+++ RGJ2/Assets/Script/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public GameObject credits;
+    public GameObject menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Credits()
+    {
+        menu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void Back()
+    {
+        menu.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Hique");
+    }
+
+}
